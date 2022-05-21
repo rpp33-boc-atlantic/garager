cd79921b21fc7e8aa41d1c555d28a9c0a2a0553a
Joann Whang <joann.whang@gmail.com>
2022-05-21 00:11:10 -0400

    remove from server - app.js and index.js...server files will be handled by Rudy with Express server setup

--- server/app.js
+++
@@ -1,2 +0,0 @@
-// this would potentially be where most of the express functionality would live (middleware, router/routes, etc.)
-// feel free to change the name of the file or consolidate with server/index.js
--- server/index.js
+++
@@ -1,2 +0,0 @@
-// this would potentially be where the server starts and listens on a particular port, i.e. app.listen(...)
-// feel free to change the name of the file or consolidate with server/app.js
