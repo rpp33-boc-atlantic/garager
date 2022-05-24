cbe08064723f8f24a2ed9f3a8237f07a2ee42b5c
Joann Whang <joann.whang@gmail.com>
2022-05-24 01:37:02 -0400

    fix: missing semicolons

--- client/src/FAQ.jsx
+++ client/src/FAQ.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 const FAQ = () => {
   return <h1>FAQ</h1>;
-}
+};
 
 export default FAQ;
--- client/src/RouterTest.jsx
+++ client/src/RouterTest.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 const RouterTest = () => {
   return <h1>RouterTest</h1>;
-}
+};
 
 export default RouterTest;
