32c038660e847b8a1b6e3753cc33c2179e312bbd
Joann Whang <joann.whang@gmail.com>
2022-05-24 01:24:58 -0400

    fix: change quotes to single quotes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,7 @@ import {
   BrowserRouter,
   Routes,
   Route,
-} from "react-router-dom";
+} from 'react-router-dom';
 import * as ReactDOM from 'react-dom/client';
 import App from './app.jsx';
 import FAQ from './FAQ.jsx';
