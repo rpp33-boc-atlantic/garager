c118f0895e90067be6a4af68986e5d79363498bd
Joann Whang <joann.whang@gmail.com>
2022-05-23 22:20:15 -0400

    install react router and following docs to implement basic router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
 import App from './app.jsx';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
