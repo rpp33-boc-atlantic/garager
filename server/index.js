5b556833028e44a33bf32a54c420fdd63bc63ce4
ChloeM <meinshac@gmail.com>
2022-05-23 23:51:43 -0400

    initial work on account routes and controllers

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const accountRouter = require('./routes/account.routes.js');
 let app = express();
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
+app.use('/account/',accountRouter);
 
 app.get('/test', (req, res) => {
   res.status(200).send({ message: 'it works!'});
 });
+
 
 module.exports = app;
--- server/routes/account.routes.js
+++ server/routes/account.routes.js
@@ -6,6 +6,13 @@ const router = require('express').Router();
 const controller = require('../controllers/account.controllers.js');
 
 // Connect controller methods to their corresponding routes
-router.get('/account/my-rentals-upcoming', controller.example.get);
+router.get('/my-rentals-upcoming', controller.upcomingRentals.get);
+
+router.get('/my-rentals-past', controller.account.get);
+
+router.get('/my-listings-earnings', controller.account.get);
+
+router.get('/my-listings', controller.account.get);
 
 module.exports = router;
+
