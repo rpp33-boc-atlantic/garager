b4bccfd1330810418f00bff8b67345caf246eeee
ChloeM <meinshac@gmail.com>
2022-05-23 22:39:21 -0400

    add account route and controller file

---
+++ server/routes/accounts.routes.js
@@ -0,0 +1,11 @@
+/* This is an example file for where we can put the ROUTES for our particular feature. The code below is just an example and will be different for each feature!
+
+file naming example: accounts.routes.js, postItem.routes.js (you can name it as you wish)
+*/
+const router = require('express').Router();
+const controller = require('../controllers/account.controllers.js');
+
+// Connect controller methods to their corresponding routes
+router.get('/account/my-rentals-upcoming', controller.example.get);
+
+module.exports = router;
