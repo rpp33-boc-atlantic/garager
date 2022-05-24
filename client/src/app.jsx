354473627be1e7237a0d3f943fce67eb37c39b0c
Joann Whang <joann.whang@gmail.com>
2022-05-24 01:55:28 -0400

    setup checkout to main app

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Outlet, Link } from 'react-router-dom';
+import Checkout from './checkout/Checkout.jsx';
 
 const App = () => {
 
@@ -11,6 +12,7 @@ const App = () => {
         <Link to='RouterTest'>RouterTest</Link>
       </nav>
       <Outlet />
+      <Checkout />
     </div>
   );
 };
--- client/src/checkout/checkout.jsx
+++ client/src/checkout/checkout.jsx
@@ -3,7 +3,10 @@ import React, { useState } from 'react';
 const Checkout = () => {
 
   return (
-    <h1>Checkout</h1>
+    <>
+      <h1>Checkout</h1>
+      <h2>Temporary placement here until it's connected to Item Details.</h2>
+    </>
   );
 };
 
