c2391a024167d7b4c7d2fba816d716eea9f713cb
Joe Mitzman <joemitz@gmail.com>
2022-05-24 20:07:56 -0700

    updated sample data

--- client/src/messages/sampleThreads.js
+++ client/src/messages/sampleThreads.js
@@ -1,24 +1,24 @@
 export default [
   {
     itemId: 3498,
-    itemName: 'wrench',
+    itemName: 'chaos magic',
     itemImageUrl: null,
     theirRole: 'owner',
-    theirUsername: 'dude bro',
+    theirUsername: 'wanda maximoff',
     theirImageUrl: null,
     lastMessage: 'what do you think of the item?',
     timeUpdated: 1653359934,
     viewed: false,
     theirMessages: [
       {
-        text: 'what do you think of the item?',
+        text: 'what do you think of my power?',
         imageUrl: null,
         timeCreated: 1653359934
       }
     ],
     myMessages: [
       {
-        text: `it's okay`,
+        text: `it's a bit much, tbh`,
         imageUrl: null,
         timeCreated: 1653359510
       }
