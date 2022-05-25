a6ad5dc91362e0298cea0325e9d363eea50ccb17
Rudy Sarmiento <83668987+rudyesar@users.noreply.github.com>
2022-05-25 16:18:45 -0700

    laid out item details component

--- client/src/itemView/ItemDetails.jsx
+++ client/src/itemView/ItemDetails.jsx
@@ -6,13 +6,37 @@ const Container = styled.div`
   background: white;
   padding: 1em;
   // align-self: start;
+  grid-template-columns: 1fr 1fr;
+  // text-indent: .8em
+  grid-auto-rows: minmax(60px, auto);
+
+  h4 {
+    margin-top: .5em;
+    margin-bottom: .4em;
+  }
+
+  .description {
+    grid-row: 2;
+    grid-column: 1 / 4;
+  }
 `;
 
 const ItemDetails = (props) => {
 
   return (
     <Container>
-      Item details: Model, brand, description
+      <div>
+        <h4>Model</h4>
+        {props.details.model}
+      </div>
+      <div>
+        <h4>Brand</h4>
+        {props.details.brand}
+      </div>
+      <div className='description'>
+        <h4>Description</h4>
+        {props.details.description}
+      </div>
     </Container>
   )
 
