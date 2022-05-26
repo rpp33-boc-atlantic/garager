5ef43469b08798ca0d28913d882cda7dc24a8838
Rudy Sarmiento <83668987+rudyesar@users.noreply.github.com>
2022-05-25 17:35:41 -0700

    implemented functionality so that suggested price option shows up only if owner has it available

--- client/src/itemView/RentForm.jsx
+++ client/src/itemView/RentForm.jsx
@@ -7,14 +7,77 @@ const Container = styled.div`
   padding: 1em;
 `;
 
-const RentForm = (props) => {
+const NYOP = styled.div``;
 
-  return (
-    <Container>
-      Rent form...
-    </Container>
-  )
+class RentForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pickUpDate: null,
+      returnDate: null,
+      suggestedPrice: null
+    };
 
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.checkFormData = this.checkFormData.bind(this);
+  }
+
+  handleChange(e) {
+    e.preventDefault();
+    this.setState({
+      [e.target.id]: e.target.value
+    }, ()=> {
+      console.log('state', this.state);
+    })
+
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if(this.checkFormData()) {
+      // call server and proceed to checkout
+      console.log('gonna make request to server');
+    }
+  }
+
+  checkFormData() {
+    if (this.state.suggestedPrice !== null && this.state.suggestedPrice.length !== 0) {
+      if (parseInt(this.state.suggestedPrice) < this.props.formInfo.minimumPrice) {
+        alert('Suggested price is too low! Don\'t be cheap!');
+        return false;
+      }
+    }
+    const pd = new Date(this.state.pickUpDate);
+    const rd = new Date(this.state.returnDate);
+
+    if (rd > pd) {
+      console.log('All good')
+      return true;
+    } else {
+      alert('Return date must follow pick up date');
+    }
+  }
+
+  render() {
+    var suggestedPrice = <NYOP><label htmlFor='suggestedPrice'>Suggested Price ($):</label> <input type='number' min="0" step="1" placeholder='Round to nearest $' id='suggestedPrice' name='suggestedPrice' onChange={this.handleChange}></input> <br></br></NYOP>;
+    var suggestedPriceLine = this.props.formInfo.nameYourOwnPrice ? suggestedPrice : null;
+
+    return (
+      <Container>
+        <form>
+          Price per day ($): {this.props.formInfo.price} <br></br>
+          <label htmlFor='pickUpDate'>Pickup Date: </label>
+          <input type='date' id='pickUpDate' name='pickUpDate' onChange={this.handleChange}></input><br></br>
+          <label htmlFor='returnDate'>Return Date:</label>
+          <input type='date' id='returnDate' name='returnDate' onChange={this.handleChange}></input><br></br>
+          {suggestedPriceLine}
+          <input type='submit' value='Rent' onClick={this.handleSubmit}></input>
+        </form>
+      </Container>
+    )
+  }
 };
 
 export default RentForm;
+
