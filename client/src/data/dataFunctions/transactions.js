1cd059dcfa8a81b1393137919b968af1fd046d21
ChloeM <meinshac@gmail.com>
2022-05-26 01:42:30 -0400

    fixed dates so that they are different but close together for transactions

--- client/src/data/dataFunctions/transactions.js
+++ client/src/data/dataFunctions/transactions.js
@@ -4,6 +4,18 @@ var owners = ['Ron Swanson', 'Leslie Knope', 'Russ Hanneman', 'Jack Barker', 'Mi
 var transactions = {};
 var fs = require('fs');
 
+const today = new Date();
+const threedaysFromNow = new Date(today);
+threedaysFromNow.setDate( threedaysFromNow.getDate() + 3);
+console.log(today, threedaysFromNow);
+
+var random = function (min = 0, max) {
+
+  var randomNum = Math.floor(Math.random() * max) + min;
+  console.log('randomNum', randomNum);
+  return randomNum;
+};
+
 
 for (var i = 0; i < 10; i ++) {
   var transaction = {};
@@ -18,10 +30,16 @@ for (var i = 0; i < 10; i ++) {
   transaction['renterName'] = owners[renterId];
   transaction['price'] = Math.floor((Math.random() * 100) + 4);
   transaction['startDate'] = new Date();
+  var firstDate = random(-10, 10);
+  console.log('first', firstDate);
+  transaction['startDate'].setDate(transaction['startDate'].getDate() + firstDate);
+  var secondDate = random(firstDate + 5, firstDate);
   transaction['dueDate'] = new Date(); // these dates need to be changed to different dates that arent too far apart from eachother
+  transaction['dueDate'].setDate(transaction['dueDate'].getDate() + secondDate);
+
   transactions[i] = transaction;
 }
 console.log(transactions);
 
-let data = JSON.stringify(transactions, null, 2);
-fs.writeFileSync('transactions.json', data);
+// let data = JSON.stringify(transactions, null, 2);
+// fs.writeFileSync('transactions.json', data);
