57c67e06e0159e657b613f5586cf3b7d55b3ed77
Thao Nguyen <thaonguyen@Thaos-MacBook-Pro.local>
2022-05-26 11:57:52 -0700

    fix lint

--- client/src/postItem/step1.jsx
+++ client/src/postItem/step1.jsx
@@ -25,71 +25,71 @@ const Step1 = (props) => {
 
   return (
     <ThemeProvider theme={theme}>
-        <React.Fragment>
-          <h3>What do you want to rent out ?</h3>
-          <Box
-            component="form"
-            sx={{'& .MuiTextField-root': { m: 1, width: '25ch' }}}
-            autoComplete="off"
-          >
-            <TextField
-              required
-              label="Title"
-              placeholder="Choose title for your post"
-              onChange={props.handleChange('title')}
-              defaultValue={props.values.title}
-              margin="normal"
-            />
-          </Box>
-          <br/>
-          <h4>Upload Photos</h4>
-          <ImageUploading
-            multiple
-            value={images}
-            onChange={onChange}
-            maxNumber={maxNumber}
-            dataURLKey="data_url"
-          >
-            {({
-              imageList,
-              onImageUpload,
-              onImageRemoveAll,
-              onImageUpdate,
-              onImageRemove,
-              isDragging,
-              dragProps,
-            }) => (
-              // write your building UI
-              <div className="upload__image-wrapper">
-                <button
-                  style={isDragging ? { color: 'red' } : undefined}
-                  onClick={onImageUpload}
-                  {...dragProps}
-                >
-                  Click or Drop here
-                </button>
-                &nbsp;
-                <button onClick={onImageRemoveAll}>Remove all images</button>
-                {imageList.map((image, index) => (
-                  <div key={index} className="image-item">
-                    <img src={image['data_url']} alt="" width="100" />
-                    <div className="image-item__btn-wrapper">
-                      <br/>
-                      <button onClick={() => onImageUpdate(index)}>Update</button>
-                      <button onClick={() => onImageRemove(index)}>Remove</button>
-                      <br/>
-                    </div>
+      <React.Fragment>
+        <h3>What do you want to rent out ?</h3>
+        <Box
+          component="form"
+          sx={{'& .MuiTextField-root': { m: 1, width: '25ch' }}}
+          autoComplete="off"
+        >
+          <TextField
+            required
+            label="Title"
+            placeholder="Choose title for your post"
+            onChange={props.handleChange('title')}
+            defaultValue={props.values.title}
+            margin="normal"
+          />
+        </Box>
+        <br/>
+        <h4>Upload Photos</h4>
+        <ImageUploading
+          multiple
+          value={images}
+          onChange={onChange}
+          maxNumber={maxNumber}
+          dataURLKey="data_url"
+        >
+          {({
+            imageList,
+            onImageUpload,
+            onImageRemoveAll,
+            onImageUpdate,
+            onImageRemove,
+            isDragging,
+            dragProps,
+          }) => (
+            // write your building UI
+            <div className="upload__image-wrapper">
+              <button
+                style={isDragging ? { color: 'red' } : undefined}
+                onClick={onImageUpload}
+                {...dragProps}
+              >
+                Click or Drop here
+              </button>
+              &nbsp;
+              <button onClick={onImageRemoveAll}>Remove all images</button>
+              {imageList.map((image, index) => (
+                <div key={index} className="image-item">
+                  <img src={image['data_url']} alt="" width="100" />
+                  <div className="image-item__btn-wrapper">
+                    <br/>
+                    <button onClick={() => onImageUpdate(index)}>Update</button>
+                    <button onClick={() => onImageRemove(index)}>Remove</button>
+                    <br/>
                   </div>
-                ))}
-              </div>
-            )}
-          </ImageUploading>
-          <Button
-            onClick={() => onClick(images)}
-          >Next</Button>
-        </React.Fragment>
-      </ThemeProvider>
-  )
-}
+                </div>
+              ))}
+            </div>
+          )}
+        </ImageUploading>
+        <Button
+          onClick={() => onClick(images)}
+        >Next</Button>
+      </React.Fragment>
+    </ThemeProvider>
+  );
+};
 
 export default Step1;
