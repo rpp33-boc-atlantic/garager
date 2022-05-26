22bec2d583951efc99d9fcd72c4ceca14054af6d
Thao Nguyen <thaonguyen@Thaos-MacBook-Pro.local>
2022-05-25 21:39:12 -0700

    add autocomplete for address, able to get all data to submit

--- client/src/postItem/step4.jsx
+++ client/src/postItem/step4.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import PlacesAutocomplete, { geocodeByAddress, geocodeByPlaceId, getLatLng } from 'react-places-autocomplete';
+
 import { ThemeProvider as MuiThemeProvider } from '@mui/material/styles';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
@@ -11,8 +13,14 @@ import theme from '../utils/theme.js';
 class Step4 extends Component {
   constructor (props) {
     super (props);
+    this.state = {
+      address: '',
+      latLng: {}
+    }
     this.continue = this.continue.bind(this);
     this.back = this.back.bind(this);
+    this.handleSelect = this.handleSelect.bind(this);
+    this.handleChangeInStep4 = this.handleChangeInStep4.bind(this);
   }
 
   continue (e) {
@@ -23,6 +31,17 @@ class Step4 extends Component {
   back (e) {
     e.preventDefault();
     this.props.changeToPrevious();
+  }
+
+  handleChangeInStep4 (address) {
+    this.setState({ address });
+  }
+
+  handleSelect (address) {
+    geocodeByAddress(address)
+      .then(results => getLatLng(results[0]))
+      .then(latLng => this.setState({ address: address, latLng: latLng}, () => { this.props.handleSelectLocation(address, latLng); }))
+      .catch(error => console.error('Error', error));
   }
 
   render () {
@@ -38,13 +57,51 @@ class Step4 extends Component {
           >
             <br/>
             <InputLabel>Pick Up Location</InputLabel>
-            <TextField
+            <PlacesAutocomplete
+              value={this.state.address}
+              onChange={this.handleChangeInStep4}
+              onSelect={this.handleSelect}
+            >
+              {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+                <div>
+                  <input
+                    {...getInputProps({
+                      placeholder: 'Search Places ...',
+                      className: 'location-search-input',
+                    })}
+                  />
+                  <div className="autocomplete-dropdown-container">
+                    {loading && <div>Loading...</div>}
+                    {suggestions.map(suggestion => {
+                      const className = suggestion.active
+                        ? 'suggestion-item--active'
+                        : 'suggestion-item';
+                      // inline style for demonstration purpose
+                      const style = suggestion.active
+                        ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                        : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                      return (
+                        <div
+                          {...getSuggestionItemProps(suggestion, {
+                            className,
+                            style,
+                          })}
+                        >
+                          <span>{suggestion.description}</span>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+              )}
+            </PlacesAutocomplete>
+            <br />
+            {/* <TextField
               placeholder="Required"
-              lable="Pick Up Location"
-              onChange={handleChange('pickUpLocation')}
-              defaultValue={values.pickUpLocation}
+              onChange={handleChange('address1')}
+              value={this.state.address}
               margin="normal"
-            />
+            /> */}
           </Box>
           <Button
             onClick={this.back}
@@ -59,3 +116,4 @@ class Step4 extends Component {
 }
 
 export default Step4;
+
