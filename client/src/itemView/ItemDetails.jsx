35da8f2be0526630c1251669158b826344621930
Rudy Sarmiento <83668987+rudyesar@users.noreply.github.com>
2022-05-27 22:18:11 -0700

    modified height of rows

--- client/src/itemView/ItemDetails.jsx
+++ client/src/itemView/ItemDetails.jsx
@@ -3,40 +3,45 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   display: grid;
-  background: white;
+  // background: white;
   padding: 1em;
   // align-self: start;
   grid-template-columns: 1fr 1fr;
   // text-indent: .8em
   grid-auto-rows: minmax(60px, auto);
+  grid-template-rows: 5em 1fr;
 
   h4 {
     margin-top: .5em;
-    margin-bottom: .4em;
+    margin-bottom: .2em;
   }
 
   .description {
     grid-row: 2;
     grid-column: 1 / 4;
   }
+  .inside {
+    // background: white;
+  }
+
 `;
 
 const ItemDetails = (props) => {
 
   return (
     <Container>
-      <div>
-        <h4>Model</h4>
-        {props.details.model}
-      </div>
-      <div>
-        <h4>Brand</h4>
-        {props.details.brand}
-      </div>
-      <div className='description'>
-        <h4>Description</h4>
-        {props.details.description}
-      </div>
+        <div className='inside'>
+          <h4>Model</h4>
+          {props.details.model}
+        </div>
+        <div className='inside'>
+          <h4>Brand</h4>
+          {props.details.brand}
+        </div>
+        <div className='description inside'>
+          <h4>Description</h4>
+          {props.details.description}
+        </div>
     </Container>
   );
 
