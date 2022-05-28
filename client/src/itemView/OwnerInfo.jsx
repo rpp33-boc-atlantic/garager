aac78a6fd8a96b1d6fa0065da96c878a77c15343
Rudy Sarmiento <83668987+rudyesar@users.noreply.github.com>
2022-05-28 00:09:22 -0700

    modified spacing in ownerInfo component

--- client/src/itemView/OwnerInfo.jsx
+++ client/src/itemView/OwnerInfo.jsx
@@ -5,10 +5,16 @@ import {Link} from 'react-router-dom';
 const Container = styled.div`
   display: grid;
   // background: white;
-  padding: 2em;
+  // padding: 2em;
+  padding-top: .5px;
+  padding-right: 2em;
+  padding-bottom: 4em;
+  padding-left: 2em;
+
   // justify-self: end;
   grid-row: 1 / 3;
   grid-template-columns: 1fr;
+  justify-content: start
 
   h2 {
     margin-top: .2em;
@@ -19,17 +25,15 @@ const Container = styled.div`
   h4 {
     margin-top: .2em;
   }
-
 `;
 
 const OwnerInfo = (props) => {
   // console.log('props here', props)
   return (
     <Container>
-
       <h2>{props.name}</h2>
       <h4>{props.owner.address}</h4>
-      <h4>Item Owner: {props.owner.name}</h4>
+      <h4>Owner: {props.owner.name}</h4>
       <h4>Member Since: {props.owner.dateJoined}</h4>
       <Link to="/Messages">
         <button>Message</button>
