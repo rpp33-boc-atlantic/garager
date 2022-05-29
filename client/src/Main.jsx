a078179518ccce0b97a238e02dbdecf1ea75d144
Wen Dai <wendai@Wens-MacBook-Pro-2.local>
2022-05-28 17:53:10 -0700

    implemented private route

--- client/src/Main.jsx
+++ client/src/Main.jsx
@@ -37,17 +37,17 @@ root.render(
         <Route path='/home' element={<Homepage />}/>
         <Route path='/login' element={<Login />} />
         <Route path='/signup' element={<Signup />} />
-        <Route path='/' exact element={<App />}>
-          <Route path='FAQ' element={<FAQ />} />
+        <Route path='/' exact element={<PrivateRoute><App /></PrivateRoute>}>
+          <Route path='FAQ' element={<PrivateRoute><FAQ /></PrivateRoute>} />
           <Route path='RouterTest' element={<RouterTest />} />
-          <Route path='Item' element={<Item />} />
-          <Route path='SearchBrowse' element={<SearchBrowse />} />
+          <Route path='Item' element={<PrivateRoute><Item /></PrivateRoute>} />
+          <Route path='SearchBrowse' element={<PrivateRoute><SearchBrowse /></PrivateRoute>} />
           <Route path='PostItem' element={<PrivateRoute><PostItem /></PrivateRoute>} />
-          <Route path='Messages' element={<Messages />} />
-          <Route path='my-listings' element={<Listings />} />
-          <Route path='my-rentals' element={<Rentals />} />
-          <Route path='/CheckoutSuccess' element={<CheckoutSuccess />} />
-          <Route path='/CheckoutCancel' element={<CheckoutCancel />} />
+          <Route path='Messages' element={<PrivateRoute><Messages /></PrivateRoute>} />
+          <Route path='my-listings' element={<PrivateRoute><Listings /></PrivateRoute>} />
+          <Route path='my-rentals' element={<PrivateRoute><Rentals /></PrivateRoute>} />
+          <Route path='/CheckoutSuccess' element={<PrivateRoute><CheckoutSuccess /></PrivateRoute>} />
+          <Route path='/CheckoutCancel' element={<PrivateRoute><CheckoutCancel /></PrivateRoute>} />
         </Route>
       </Routes>
     </UserAuthContextProvider>
