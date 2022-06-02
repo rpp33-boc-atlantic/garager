0c12d7caae3588a9885f3d5f804d7fc55d6f0531
Nick Gerrard <gerrard.nick@gmail.com>
2022-06-02 09:22:33 -0700

    added password to config file

--- server/database/database.js
+++ server/database/database.js
@@ -1,2 +1,20 @@
-// this would be where we can connect to the database
-// also feel free to change the file name!
+require('dotenv').config();
+const RDS_PASSWORD = require('../../config.js').RDS_PASSWORD;
+const { Client } = require('pg');
+
+
+const client = new Client({
+  user: 'garagerAdmin',
+  host: 'garager.c11jhqw8tzhf.us-east-1.rds.amazonaws.com',
+  database: 'garager',
+  password: RDS_PASSWORD,
+  port: 5432
+});
+
+client.connect((err, res) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('connected to GARAGER database');
+  }
+});
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const accountRouter = require('./routes/account.routes.js');
+require('./database/database.js');
 
 
 const bodyParser = require('body-parser');
