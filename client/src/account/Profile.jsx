f1d309fce999b5eda1d505f16fa1a05e2f18b390
ChloeM <meinshac@gmail.com>
2022-06-03 15:27:40 -0400

    clean up profile component

--- client/src/account/Profile.jsx
+++ client/src/account/Profile.jsx
@@ -1,15 +1,39 @@
 
-import * as React from 'react';
-import ReactDOM from 'react-dom';
-import Button from '@mui/material/Button';
-import Container from '@mui/material/Container';
+import React from 'react';
 import RentalTabs from './RentalTabs.jsx';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import ThemeProvider from 'react-bootstrap/ThemeProvider';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import ProfileCard from './ProfileCard.jsx';
+import ProfileTabs from './ProfileTabs.jsx';
 
-
+// var transactions = require('../data/dataFunctions/transactions.json');
+var users = require('../data/dataFunctions/users.json');
+var user = users[0];
 export default function Profile () {
-  // return <Button variant="contained">Hello World</Button>;
-  // return <BasicTabs listings = {false} m='auto' earnings = {454} rentedItems = {3}/>;
-  return <h1>Profile Page</h1>;
 
+  return ( <ThemeProvider breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}>
+
+    <Container fluid>
+      <Row> profile  </Row>
+      <Row>
+        <Col></Col>
+        <Col>
+          <ProfileCard user= {user}></ProfileCard>
+          {/* {/* <Image thumbnail = {true} width = {600}roundedCircle = {true} fluid = {true} src = {user.userPhoto} /> */}
+        </Col >
+        <Col xs={8}>
+          <ProfileTabs user ={user}></ProfileTabs> (wider)
+        </Col>
+        <Col></Col>
+      </Row>
+      <Row>
+      </Row>
+      <Row></Row>
+    </Container>
+  </ThemeProvider>
+  );
 }
 
