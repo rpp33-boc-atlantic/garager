680b97697a68e9921b57e004d9c62bdc8e3bd31c
Wen Dai <wendai@Wens-MacBook-Pro-2.local>
2022-06-03 00:17:24 -0700

    minor change

--- client/src/authentication/privateRoute.jsx
+++ client/src/authentication/privateRoute.jsx
@@ -8,7 +8,6 @@ const PrivateRoute = ({children}) => {
   let { user } = useUserAuth();
   //console.log('Check user in PrivateRoute: ', user);
   if (!user) {
-    alert('Please sign in first!');
     return <Navigate to='/login'/>;
   }
   return children;
