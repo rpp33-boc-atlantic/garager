30259c8d756d6790bc63a323d726f4804d71d66a
Thao Nguyen <thaonguyen@Thaos-MacBook-Pro.local>
2022-06-03 01:32:06 -0700

    comment out local postgres

--- server/database/database.js
+++ server/database/database.js
@@ -2,19 +2,19 @@ require('dotenv').config();
 const RDS_PASSWORD = require('../../config.js').RDS_PASSWORD;
 const { Client } = require('pg');
 
-// const client = new Client({
-//   user: 'garagerAdmin',
-//   host: 'garager.c11jhqw8tzhf.us-east-1.rds.amazonaws.com',
-//   database: 'garager',
-//   password: RDS_PASSWORD,
-//   port: 5432
-// });
-
 const client = new Client({
-  host: 'localhost',
+  user: 'garagerAdmin',
+  host: 'garager.c11jhqw8tzhf.us-east-1.rds.amazonaws.com',
   database: 'garager',
+  password: RDS_PASSWORD,
   port: 5432
 });
+
+// const client = new Client({
+//   host: 'localhost',
+//   database: 'garager',
+//   port: 5432
+// });
 
 client.connect((err, res) => {
   if (err) {
