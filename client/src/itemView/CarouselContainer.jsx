d37545e9592a92155a6b603acf825647caf2c53e
Rudy Sarmiento <83668987+rudyesar@users.noreply.github.com>
2022-06-03 20:40:39 -0700

    Working on css of carousel container

--- client/src/itemView/CarouselContainer.jsx
+++ client/src/itemView/CarouselContainer.jsx
@@ -5,35 +5,60 @@ import { Carousel } from 'react-bootstrap';
 const Container = styled.div`
   display: grid;
   background: #eee;
-  padding-left: 1em;
-  padding-right: 1em;
+  // padding-left: 1em;
+  // padding-right: 1em;
   grid-row: 1 / 3;
   // grid-auto-rows: minmax(60px, auto);
   // grid-template-rows: .5em 1fr;
-  max-height: 2em
+  // max-height: 100px
 
-  img {
-    max-width: 100%;
-    max-height: 100%;
-    display: block;
-    justify-items:center
-  }
+  // img {
+  //   max-width: 100%;
+  //   max-height: 100%;
+  //   display: block;
+  //   justify-items:center
+  // }
+
+  // .carousel .item {
+  //   height: 100px;
+  // }
+
+  // .item img {
+  //   position: absolute;
+  //   top: 0;
+  //   left: 0;
+  //   min-height: 100px;
+  // }
+
+  // NEW
+  // display: flex;
+  // height: 30em;
+  // width: 50em;
 `;
 
 const CarouselContainer = (props) => {
 
   const carouselItem = props.images.map((image, i) =>
-    <Carousel.Item key={i}>
+  <Carousel.Item key={i} >
+
+    {/* </Carousel.Item><Carousel.Item key={i} style={{height: '30em'}}> */}
       <img
-        className='d-block w-50'
+        className='d-block w-100 img-fluid'
+
+        // className='img-fluid'
+        // className='d-block w-50'
         src={image}
+        style={{height: '30em', width: '50em'}}
+        // style={{position: 'absolute'}}
       />
     </Carousel.Item>
   );
 
   return (
     <Container>
-      <Carousel interval={null} fade={true} variant="dark">
+      {/* <Carousel interval={null} fade={true} variant="dark" style={{height: '100px'}}> */}
+
+      <Carousel interval={null} fade={true} variant="dark" >
         {carouselItem}
       </Carousel>
     </Container>
