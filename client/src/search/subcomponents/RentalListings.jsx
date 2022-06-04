b5ca7b4057fc04f8a83f9227e1d014b9ce29aeb5
Nick Gerrard <gerrard.nick@gmail.com>
2022-06-04 11:56:11 -0700

    added functionality to go to item display view

--- client/src/search/subcomponents/RentalListings.jsx
+++ client/src/search/subcomponents/RentalListings.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class RentalListings extends React.Component {
   constructor (props) {
@@ -27,22 +28,24 @@ class RentalListings extends React.Component {
     return (
       <div id="rental-listings">
         {this.props.rentals.map((rental) =>
-          <div className="rental-listing" key={rental.id}>
-            <img src={rental.details.image[0]}></img>
-            <div className="listing-text">
-              <div className="listing-header">
-                <span className="rental-title">{rental.details.category} | {this.props.query ?
-                  this.highlightKeywordSearch(this.props.query, rental.name) :
-                  rental.name
-                }</span>
-                <span className="rental-price">{rental.details.price}/day</span>
+          <Link to={`Item/id=${rental.id}`} key={rental.id} className="go-to-item-listing">
+            <div className="rental-listing">
+              <img src={rental.details.image[0]}></img>
+              <div className="listing-text">
+                <div className="listing-header">
+                  <span className="rental-title">{rental.details.category} | {this.props.query ?
+                    this.highlightKeywordSearch(this.props.query, rental.name) :
+                    rental.name
+                  }</span>
+                  <span className="rental-price">{rental.details.price}/day</span>
+                </div>
+                <p>{this.props.query ?
+                  this.highlightKeywordSearch(this.props.query, rental.details.description) :
+                  rental.details.description
+                }</p>
               </div>
-              <p>{this.props.query ?
-                this.highlightKeywordSearch(this.props.query, rental.details.description) :
-                rental.details.description
-              }</p>
             </div>
-          </div>
+          </Link>
         )}
       </div>
     );
