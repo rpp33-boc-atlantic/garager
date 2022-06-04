f2dd70ba123c787000d5ef96fa7c066c7ad4fe6b
Nick Gerrard <gerrard.nick@gmail.com>
2022-06-04 10:05:42 -0700

    added sort functionality to browse by popularity, price and aphabetical order

--- client/src/data/exampleRentals.js
+++ client/src/data/exampleRentals.js
@@ -5,7 +5,7 @@ const rentalListings = [
     details: {
       category: 'Hand Tools',
       description: 'A simple hammer for your basic needs. Works great to drive nails and break things into small pieces',
-      price: '500',
+      price: 500,
       image: ['https://www.svgrepo.com/show/105084/hammer.svg'],
       location: {
         lat: 37.38860,
@@ -15,7 +15,8 @@ const rentalListings = [
         startDate: 1654066800000,
         endDate: 1656572400000,
         rentedDates: [[1656140400000, 1656140400000]]
-      }
+      },
+      transactions: 5
     }
   },
   {
@@ -24,7 +25,7 @@ const rentalListings = [
     details: {
       category: 'Events',
       description: 'Weber charcoal bbq grill. No propane needed, just light up some coal and grill! Lighter fluid and charcoal briquettes not included in rental.',
-      price: '20000',
+      price: 20000,
       image: ['https://www.svgrepo.com/show/297780/barbecue-bbq.svg'],
       location: {
         lat: 37.36965,
@@ -34,7 +35,8 @@ const rentalListings = [
         startDate: 1454066800000,
         endDate: 1654066800000,
         rentedDates: [1656140400000]
-      }
+      },
+      transactions: 10
     }
   },
   {
@@ -43,7 +45,7 @@ const rentalListings = [
     details: {
       category: 'Cleaning',
       description: 'have dirty floors? This mop has a sturdy handle and is ready to clean all of your floors. Recommended to be used with a mop bucket which can also be rented',
-      price: '1000',
+      price: 1000,
       image: ['https://www.svgrepo.com/show/321914/broom.svg'],
       location: {
         lat: 38.58190,
@@ -53,7 +55,8 @@ const rentalListings = [
         startDate: 1754066800000,
         endDate: 1856572400000,
         rentedDates: [1656140400000]
-      }
+      },
+      transactions: 0
     }
   },
   {
@@ -62,7 +65,7 @@ const rentalListings = [
     details: {
       category: 'Sports Equipment',
       description: 'Two six foot wide pop up soccer goals. Great to play outside at the park, in a lawn, or even in the driveway! Comes with 4 stakes per goal to keep them mounted to the ground. Excellent for drilling shots or a quick pick up game at the park',
-      price: '2500',
+      price: 2500,
       image: ['https://www.svgrepo.com/show/142452/soccer-ball.svg'],
       location: {
         lat: 34.00984,
@@ -72,7 +75,8 @@ const rentalListings = [
         startDate: 1654066800000,
         endDate: 1656572400000,
         rentedDates: [[1656140400000, 1656140400000]]
-      }
+      },
+      traansactions: 20
     }
   }
 ];
