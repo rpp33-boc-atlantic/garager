b507a39e0a16fa817b50c9984f8cb69c730a39b8
Wen Dai <wendai@Wens-MacBook-Pro-2.local>
2022-06-04 16:33:05 -0700

    added unique constraint on email in users table

--- server/models/auth.models.js
+++ server/models/auth.models.js
@@ -3,13 +3,13 @@ const client = require('../database/database.js');
 module.exports = {
   registerUser: {
     post: async (firstName, lastName, email, callback) => {
-      // get stuff from database
       let registerUserInfo = `INSERT INTO users (firstName, lastName, email)
       VALUES (${firstName}, ${lastName}, ${email})
-      ON CONFLICT (email)
-      DO NOTHING`;
-      //TODO: is it client.query? probably different than using pool
-      await client.query(registerUserInfo, (err, res) => {
+      ON CONFLICT
+      DO NOTHING
+      RETURNING user_id`;
+
+      await pool.query(registerUserInfo, (err, res) => {
         if (err) {
           callback(err, null);
         } else {
