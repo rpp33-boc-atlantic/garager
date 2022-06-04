a35e6f76c7670587b1125c8288c4a8ce3ad33794
Nick Gerrard <gerrard.nick@gmail.com>
2022-06-03 23:02:37 -0700

    starting to implement browse sort features

--- client/src/search/subcomponents/Browse.jsx
+++ client/src/search/subcomponents/Browse.jsx
@@ -3,9 +3,23 @@ import React from 'react';
 import RentalListings from './RentalListings.jsx';
 
 const Browse = (props) => {
+
+  const [sortBy, setSortBy] = useState('');
+
+  const handleBrowseSearch = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <div id="browse-column">
       <span>RENTALS NEAR YOU</span>
+      <div id="browse-sort">
+        <span>Sort items by </span>
+        <select name="browse-sort-options" id="sort-dropdown" onChange={handleBrowseSort}>
+          <option value="price | low > high"></option>
+          <option value="price | high > low"></option>
+        </select>
+      </div>
       <RentalListings
         rentals={props.rentals}
         query={props.query}
