a6bfeba1d3a505d316e342ce02296b279f2b90ba
Rudy Sarmiento <83668987+rudyesar@users.noreply.github.com>
2022-06-03 20:38:53 -0700

    modified code to grab id passed through params

--- client/src/itemView/wrapper.jsx
+++ client/src/itemView/wrapper.jsx
@@ -7,6 +7,7 @@ import OwnerInfo from './OwnerInfo.jsx';
 import RentForm from './RentForm.jsx';
 import sampleItemData from './sampleItemData.js';
 import axios from 'axios';
+import { useUserAuth } from '../context/UserAuthContext.jsx';
 
 const Container = styled.div`
   display: grid;
@@ -15,38 +16,75 @@ const Container = styled.div`
   padding: 1em;
   grid-template-columns: 3fr 2fr;
   grid-gap: 1em;
-  grid-auto-rows: minmax(2em, auto);
+  grid-auto-rows: minmax(1fr, auto);
   justify-items: stretch;
   // align-items: center;
-
-
-  // grid-template-rows:40px 40px;
+  // grid-template-rows: 1fr .5fr 2fr .5fr;
 `;
 
 const Item = (props) => {
   const fakeProps = sampleItemData.option1;
-  const [itemData, setData] = useState({});
+  const [itemData, setData] = useState({  itemID: null,
+    name: '',
+    brand: '',
+    model: '',
+    description: '',
+    availableFrom: '',
+    availableTo: '',
+    availability: null,
+    price: null,
+    nameYourOwnPrice: null,
+    minimumPrice: null,
+    images: [],
+    rangesBooked: [],
+    owner: {
+      id: null,
+      name: '',
+      email: '',
+      dateJoined: '',
+      address: ''
+    }});
   let { id } = useParams();
+  const { user } = useUserAuth();
+  console.log('this should be the user email', user.email);
   console.log('this is the item id passed through params', id);
+
   useEffect(() => {
-    // console.log('this is the id in params', id)
-    axios.get('/item/ItemInfo', {
+    let mounted = true;
+    axios.get('/item/itemData', {
       params: {
         ID: 12345,
         name: 'ladder'
       }
-    }, [])
+    })
       .then(response => {
-        console.log('GETTING THE ITEM INFO');
-        return setData(fakeProps);
-      })
-      .then(response => {
-        console.log('state right here', itemData);
+        if(mounted) {
+          console.log('passing thru heres')
+          setData(fakeProps);
+        }
       })
       .catch(error => {
         console.log('ERROR IN GETTING THE ITEM INFO', error);
       });
+      return () => mounted = false;
   });
+
+  const deleteItem = () => {
+    console.log('gonna delete item')
+    axios.delete('/item/itemData', {
+      data: {
+        ID: 12345
+      }
+    })
+    .then(response => {
+      console.log('item has been deleted');
+    })
+    .catch(error => {
+      console.log('error in deleting item', error);
+    })
+  };
+
+  const deleteButton = user.email === itemData.owner.email ? <button onClick={deleteItem}>Delete Item</button> : null;
 
   return (
     <div>
@@ -55,12 +93,16 @@ const Item = (props) => {
         <OwnerInfo className='owner' name={fakeProps.name} owner={fakeProps.owner} />
         <ItemDetails className='details' details={fakeProps}/>
         <RentForm className='form' itemInfo={fakeProps} />
+        {deleteButton}
       </Container>
     </div>
   );
 };
 
 export default Item;
+
+
+
 
 
 
