f22df459abc206bb0688d74fa6d996ed7f20bb89
Thao Nguyen <thaonguyen@Thaos-MacBook-Pro.local>
2022-06-07 16:18:02 -0700

    change date, photo format to fit with adjustment in database

--- server/database/database.js
+++ server/database/database.js
@@ -2,17 +2,11 @@ require('dotenv').config();
 const RDS_PASSWORD = require('../../config.js').RDS_PASSWORD;
 const { Pool } = require('pg');
 
-// const pool = new Pool({
-//   user: 'garagerAdmin',
-//   host: 'garager.c11jhqw8tzhf.us-east-1.rds.amazonaws.com',
-//   database: 'garager',
-//   password: RDS_PASSWORD,
-//   port: 5432
-// });
-
 const pool = new Pool({
-  host: 'localhost',
+  user: 'garagerAdmin',
+  host: 'garager.c11jhqw8tzhf.us-east-1.rds.amazonaws.com',
   database: 'garager',
+  password: RDS_PASSWORD,
   port: 5432
 });
 
