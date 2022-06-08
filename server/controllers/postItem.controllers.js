8349549c91db9cf5a330fa23989342c824eb009a
Thao Nguyen <thaonguyen@Thaos-MacBook-Pro.local>
2022-06-07 19:12:57 -0700

    switch branch

--- server/controllers/postItem.controllers.js
+++ server/controllers/postItem.controllers.js
@@ -1,11 +1,11 @@
 /* eslint-disable camelcase */
 const models = require('../models/postItem.models.js');
-const useMain = require('../../client/src/context/MainContext.jsx');
+// const useMain = require('../../client/src/context/MainContext.jsx');
 
 module.exports = {
   user_id: {
     post: async (req, res) => {
-      let userID = useMain() || 1;
+      let userID = 1; // useMain();
       let item_posted = await models.user_id.post(userID, req.body);
       if (item_posted) {
         res.sendStatus(201);
