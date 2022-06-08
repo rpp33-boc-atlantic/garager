999ce6af9d16c7276fd6e8a7d6a54bef107e2fd0
Thao Nguyen <thaonguyen@Thaos-MacBook-Pro.local>
2022-06-07 18:57:07 -0700

    add userid from main context

--- server/controllers/postItem.controllers.js
+++ server/controllers/postItem.controllers.js
@@ -1,10 +1,11 @@
 /* eslint-disable camelcase */
 const models = require('../models/postItem.models.js');
+const useMain = require('../../client/src/context/MainContext.jsx');
 
 module.exports = {
   user_id: {
     post: async (req, res) => {
-      let userID = req.params.user_id || 1;
+      let userID = useMain() || 1;
       let item_posted = await models.user_id.post(userID, req.body);
       if (item_posted) {
         res.sendStatus(201);
