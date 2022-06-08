cd6a008e2b15ccf97a352fe8a8b555760ff0bbcc
Wen Dai <wendai@Wens-MacBook-Pro-2.local>
2022-06-08 13:06:17 -0700

    deleted comments in database.js  Please enter the commit message for your changes. Lines starting

--- server/database/database.js
+++ server/database/database.js
@@ -9,13 +9,7 @@ const pool = new Pool({
   password: RDS_PASSWORD,
   port: 5432
 });
-/*const pool = new Pool({
-  user: 'root',
-  host: 'localhost',
-  database: 'garager',
-  password: 'password',
-  port: 5432
-});*/
+
 
 pool.on('error', (err) => {
   console.error('Unexpected error on idle client', err);
