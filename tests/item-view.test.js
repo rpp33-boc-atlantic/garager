4d19fe232d97259e4629d3e31defdc4b276eb99a
Rudy Sarmiento <83668987+rudyesar@users.noreply.github.com>
2022-06-09 00:53:57 -0700

    started working on tests

--- tests/item-view.test.js
+++ tests/item-view.test.js
@@ -1,4 +1,31 @@
 test('should display something', () => {
 
+  // const request = require('supertest');
+  // const server = require('../server/index.js');
+
+  // describe('Test Item View', () => {
+
+  //   beforeAll(done => {
+  //     done();
+  //   });
+
+  //   test('Sending GET request to /item/itemData with appropriate params', async() => {
+  //     await request(server)
+  //     .get('/item/itemData')
+  //     .query({
+  //       params: {
+  //         ID: 11
+  //       }
+  //     })
+  //     .expect(200)
+  //   });
+  // });
+
+
+
+
+
+
+
   expect('Item-view!').toMatch(/^Item(.*)/);
-});
+// });
