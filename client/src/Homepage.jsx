4f240a1fba7dadd28414a3ad631b51ed41eb4c00
Wen Dai <wendai@Wens-MacBook-Pro-2.local>
2022-06-09 16:14:36 -0700

    added background image

--- client/src/Homepage.jsx
+++ client/src/Homepage.jsx
@@ -5,7 +5,7 @@ import image from './img/hal-gatewood-v7WyjiyXNr4-unsplash.jpg';
 const Homepage = () => {
   return (
     <div>
-      <img style={{ width: '100%', height: 'auto', }} alt='tools image' src={image}></img>
+      <img style={{ width: '100%', height: 'auto'}} alt='tools image' src={image}></img>
     </div>
   );
 };
