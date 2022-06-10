afe1a311441e1c871f19be2023280079a15017bb
Rudy Sarmiento <83668987+rudyesar@users.noreply.github.com>
2022-06-09 18:00:19 -0700

    added a test for get route

--- tests/item-view.test.js
+++ tests/item-view.test.js
@@ -1,31 +1,33 @@
-test('should display something', () => {
+// test('should display something', () => {
 
-  // const request = require('supertest');
-  // const server = require('../server/index.js');
+  const request = require('supertest');
+  const server = require('../server/index.js');
 
-  // describe('Test Item View', () => {
+  describe('Test Item View', () => {
 
-  //   beforeAll(done => {
-  //     done();
-  //   });
+    beforeAll(done => {
+      done();
+    });
 
-  //   test('Sending GET request to /item/itemData with appropriate params', async() => {
-  //     await request(server)
-  //     .get('/item/itemData?ID=11')
-  //     // .query({
-  //     //   params: {
-  //     //     ID: 11
-  //     //   }
-  //     // })
-  //     .expect(200)
-  //   });
-  // });
+    test('Sending GET request to /item/itemData with appropriate params', async() => {
+      await request(server)
+      .get('/item/itemData/ID=11')
+      .expect(200)
+    });
 
+    test('Sending GET request to /item/itemData with item number that does not exist', async() => {
+      await request(server)
+      .get('/item/itemData/ID=7799')
+      .expect(404)
+    });
 
-
-
+  });
 
 
 
-  expect('Item-view!').toMatch(/^Item(.*)/);
-});
+
+
+
+
+//   expect('Item-view!').toMatch(/^Item(.*)/);
+// });
