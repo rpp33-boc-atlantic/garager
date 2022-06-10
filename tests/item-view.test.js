7a776c9ba985a2809a8f50d24173a670d7bf6d73
Rudy Sarmiento <83668987+rudyesar@users.noreply.github.com>
2022-06-10 09:43:29 -0700

    tests WIP

--- tests/item-view.test.js
+++ tests/item-view.test.js
@@ -5,24 +5,37 @@ const server = require('../server/index.js');
 
 describe('Test Item View', () => {
 
-  beforeAll(done => {
-    done();
-  });
+  // it ('GET item data request /item/ItemData', (done) => {
+  //   request(server)
+  //     .get('/item/itemData/ID=16661')
+  //     .expect(404)
+  //     .end((err, res) => {
+  //       if (err) { return done(err); }
+  //       return done();
+  //     });
 
-  test('Sending GET request to /item/itemData with appropriate params', async() => {
-    await request(server)
-      .get('/item/itemData/ID=11')
-      .expect(200);
-  });
+  // })
 
-  test('Sending GET request to /item/itemData with item number that does not exist', async() => {
-    await request(server)
-      .get('/item/itemData/ID=hh')
-      .expect(404)
-      .then((response) => {
-        expect(response.body.message).toEqual('Error 404 Item Not Found');
-      });
-  });
+
+
+  // beforeAll(done => {
+  //   done();
+  // });
+
+  // test('Sending GET request to /item/itemData with item number that does not exist', async() => {
+  //   await request(server)
+  //     .get('/item/itemData/ID=16661')
+  //     .expect(404)
+  //     // .then((response) => {
+  //     //   expect(response.body.message).toEqual('Error 404 Item Not Found');
+  //     // });
+  // });
+
+  // test('Sending GET request to /item/itemData with appropriate params', async() => {
+  //   await request(server)
+  //     .get('/asdfasdfs')
+  //     .expect(500);
+  // });
   // afterAll(done => {
   //   server.close();
   //   done();
