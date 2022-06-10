c5834a1c9e6487bcf0aabcbc68af914ee7a358ab
Joann Whang <joann.whang@gmail.com>
2022-06-10 02:20:13 -0400

    tests for stripe account setup

--- tests/checkout.test.js
+++ tests/checkout.test.js
@@ -11,10 +11,10 @@ const { Stripe } = require('stripe');
 //   retrieve: details_submitted,
 // };
 
-describe.skip('CHECKOUT SESSION', () => {
+describe('CHECKOUT SESSION', () => {
   it('returns a 500 error if no stripe account is associated with an owner', async () => {
     await supertest(server).post('/checkout/create-session')
-      .send({ ownerID: 11, dateRange: ['2022-07-23T04:00:00.000Z', '2022-07-25T04:00:00.000Z'] })
+      .send({ ownerID: 13, dateRange: ['2022-07-23T04:00:00.000Z', '2022-07-25T04:00:00.000Z'] })
       .expect(500)
       .then((res) => {
         expect(res.text).toEqual('Item owner has not setup a Stripe Account');
@@ -123,50 +123,71 @@ describe('STRIPE ACCOUNT SETUP', () => {
         expect(res.body.url).toEqual('http://localhost:3000/Stripe-Account-Setup');
       });
   });
-  // it('returns a 500 error if stripe account has not been verified', async () => {
-  //   const charges_enabled = jest.fn(() => ({
-  //     charges_enabled: false,
-  //   }));
-    
-  //   Stripe.prototype.accounts = {
-  //     retrieve: charges_enabled,
-  //   };
-    
-  //   await supertest(server).post('/checkout/create-session')
-  //     .send({ ownerID: 8, dateRange: ['2022-07-23T04:00:00.000Z', '2022-07-25T04:00:00.000Z'] })
-  //     .expect(500)
-  //     .then((res) => {
-  //       expect(res.text).toEqual('Item owner has an incomplete Stripe Account Setup');
-  //     });
-  // });
 
-  // it('returns 200 if item owner has a stripe account', async () => {
-  //   const bothTrue = jest.fn(() => ({
-  //     charges_enabled: true,
-  //     details_submitted: true,
-  //   }));
+  it('should return "incomplete" if user has not started the setup process', async () => {
+    await supertest(server).get('/checkout/check-account-completion')
+      .query({ userID: 12 })
+      .expect(200)
+      .then((response) => {
+        expect(response.text).toEqual('Incomplete - please create an account.');
+      });
+  });
+ 
+  it('should return "in-progress" if user has not submitted all the details for the account', async () => {
+    const details_submitted = jest.fn(() => ({
+      details_submitted: false,
+    }));
     
-  //   Stripe.prototype.accounts = {
-  //     retrieve: bothTrue,
-  //   };
+    Stripe.prototype.accounts = {
+      retrieve: details_submitted,
+    };
+    
+    await supertest(server).get('/checkout/check-account-completion')
+      .query({ userID: 8 })
+      .expect(200)
+      .then((response) => {
+        expect(response.text).toEqual('In-progress - please continue to fill out the details to setup your account.');
+      });
+  });
 
-  //   const checkoutSuccess = jest.fn(() => ({
-  //     url: 'localhost:3000/CheckoutSuccess'
-  //   }));
+  it('should return "nearly there" if user has not verified the account', async () => {
+    const charges_enabled = jest.fn(() => ({
+      charges_enabled: false,
+      details_submitted: true,
+    }));
     
-  //   Stripe.prototype.checkout = {
-  //     sessions: {
-  //       create: checkoutSuccess,
-  //     }
-  //   }; 
+    Stripe.prototype.accounts = {
+      retrieve: charges_enabled,
+    };
     
-  //   await supertest(server).post('/checkout/create-session')
-  //     .send({ ownerID: 5, dateRange: ['2022-07-23T04:00:00.000Z', '2022-07-25T04:00:00.000Z'], rate: 55.00 })
-  //     .expect(200);
-  // });
+    await supertest(server).get('/checkout/check-account-completion')
+      .query({ userID: 8 })
+      .expect(200)
+      .then((response) => {
+        expect(response.text).toEqual('Nearly there! Stripe is currently verifying your details. In a few minutes, please click the button to complete the final steps. This may take a few updates to finalize the account.');
+      });
+  });
+
+  it('should return "complete" if user has filled in all the account details and verification steps', async () => {
+    const charges_enabled = jest.fn(() => ({
+      charges_enabled: true,
+      details_submitted: true,
+    }));
+    
+    Stripe.prototype.accounts = {
+      retrieve: charges_enabled,
+    };
+    
+    await supertest(server).get('/checkout/check-account-completion')
+      .query({ userID: 5 })
+      .expect(200)
+      .then((response) => {
+        expect(response.text).toEqual('complete');
+      });
+  });
 });
 
-describe.skip('TEST SETUP', function () {
+describe('TEST SETUP', function () {
   it('should display something', () => {
     expect('Checkout!').toMatch(/^Checkout(.*)/);
   });
