cb33e7c80c117e1a114190859baf9244312dadd2
Wen Dai <wendai@Wens-MacBook-Pro-2.local>
2022-06-10 20:10:39 -0700

    for switching branch

--- tests/authTest/Login.test.js
+++ tests/authTest/Login.test.js
@@ -3,10 +3,9 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Login from '../../client/src/authentication/authenticationSample.jsx';
 
-describe('Login Page Test', ()=>{
-  let container;
+describe('Login Form', ()=>{
   beforeEach(() => {
-    container = render(<Login />).container;
+    const {container} = render(<Login />).container;
   });
   test('renders "Log in " title ', ()=>{
     expect(screen.getByText('Log In')).toBeInTheDocument();
